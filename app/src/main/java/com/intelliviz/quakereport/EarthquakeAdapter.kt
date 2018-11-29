9595b08b89a31ae5de8d0ca561cef6737ee5627b
Ed Muhlestein <emuhlestein@yahoo.com>
2018-11-29 17:47:56 -0600

    Add colored magnitude circles.

--- app/src/main/java/com/intelliviz/quakereport/EarthquakeAdapter.kt
+++ app/src/main/java/com/intelliviz/quakereport/EarthquakeAdapter.kt
@@ -1,11 +1,14 @@
 package com.intelliviz.quakereport
 
 import android.content.Context
+import android.graphics.drawable.GradientDrawable
+import android.support.v4.content.ContextCompat
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.earthquake_list_item.view.*
+
 
 class EarthquakeAdapter(val context: Context, val items: MutableList<Earthquake>) :
         RecyclerView.Adapter<EarthquakeAdapter.ViewHolder>() {
@@ -21,7 +24,7 @@ class EarthquakeAdapter(val context: Context, val items: MutableList<Earthquake>
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val earthquake = getItem(position)
-        holder.bind(earthquake)
+        holder.bind(earthquake, context)
     }
 
     fun addAll(earthquakes: List<Earthquake>) {
@@ -41,12 +44,44 @@ class EarthquakeAdapter(val context: Context, val items: MutableList<Earthquake>
         val dateTextView = view.date
         val timeTextView = view.time
 
-        fun bind(earthquake: Earthquake) {
+        fun bind(earthquake: Earthquake, context: Context) {
             magnitudeTextView.text = earthquake.magnitude.toString()
             distanceTextView.text = earthquake.distance
             cityTextView.text = earthquake.city
             dateTextView.text = earthquake.date
             timeTextView.text = earthquake.time
+            val magnitudeCircle = magnitudeTextView.getBackground() as GradientDrawable
+            var magnitudeColor = getMagnitudeColor(earthquake.magnitude)
+            magnitudeColor = ContextCompat.getColor(context, magnitudeColor)
+            magnitudeCircle.setColor(magnitudeColor)
+
+        }
+
+        private fun getMagnitudeColor(magnitude: Double): Int {
+
+            if(magnitude <= 1.0) {
+                return R.color.magnitude1
+            } else if(magnitude <= 2.0) {
+                return R.color.magnitude2
+            } else if(magnitude <= 3.0) {
+                return R.color.magnitude3
+            } else if(magnitude <= 4.0) {
+                return R.color.magnitude4
+            } else if(magnitude <= 5.0) {
+                return R.color.magnitude5
+            } else if(magnitude <= 6.0) {
+                return R.color.magnitude6
+            } else if(magnitude <= 7.0) {
+                return R.color.magnitude7
+            } else if(magnitude <= 8.0) {
+                return R.color.magnitude8
+            } else if(magnitude <= 9.0) {
+                return R.color.magnitude9
+            } else {
+                return R.color.magnitude10plus
+            }
         }
     }
+
+
 }
